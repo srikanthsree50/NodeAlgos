f3f5cfdc3ca7d17de1e1e5c6b779bd101bace79f
srikanthsree50 <srikanthsree50@gmail.com>
2023-12-05 19:21:19 +0530

    new changes

--- countDistinctValues.js
+++ countDistinctValues.js
@@ -3,7 +3,7 @@ if(arr.length === 0) return 0;
 var result =[]
 var i =0 ;
 for (var j = 0; j < arr.length; j++) {
-    
+   
     if(arr[i] !== arr[j]){
         i++;
         arr[i] = arr[j];
@@ -11,4 +11,4 @@ for (var j = 0; j < arr.length; j++) {
 }
 return i + 1
 }
-console.log(distinctValues([1,2,2,3,4,5,5]))
+console.log(countDistinctValues([1,2,2,3,4,5,5]))
