b420710da7fee03e53af3241c2c73c1da51947d0
srikanthsree50 <srikanthsree50@gmail.com>
2023-12-05 16:12:20 +0530

    finalize changes

--- charCount.js
+++ charCount.js
@@ -3,15 +3,28 @@
 
 const charCount = function count(str) {
     var result = {};
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        if(result[char] > 0){
-            result[char]++
+    for (var char of str) {
+        char = char.toLowerCase();
+        if(isAlphanumeric(char)){
+       // if(/[a-z]/.test(char)){
+            // if(result[char] > 0){
+            //     result[char]++
+            // }
+            // else{
+            //     result[char] =1
+            // }
+            result[char] = ++result[char] || 1 ;
         }
-        else{
-            result[char] =1
-        }
+      
     }
     return result;
+}
+
+function isAlphanumeric(char){
+    var code = char.charCodeAt(0);
+    if(!(code > 47 && code < 58) && !(code > 64 && code < 91) && !(code > 96 && code < 123)){
+        return false
+    }
+    return true
 }
 console.log(charCount('abccc2'))
