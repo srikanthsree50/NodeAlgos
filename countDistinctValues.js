5e648e292fba9633844ebf057c06ca9896fd33c1
srikanthsree50 <srikanthsree50@gmail.com>
2023-12-05 19:18:37 +0530

    count distinct values

---
+++ countDistinctValues.js
@@ -0,0 +1,14 @@
+function countDistinctValues(arr){
+if(arr.length === 0) return 0;
+var result =[]
+var i =0 ;
+for (var j = 0; j < arr.length; j++) {
+    
+    if(arr[i] !== arr[j]){
+        i++;
+        arr[i] = arr[j];
+    }
+}
+return i + 1
+}
+console.log(distinctValues([1,2,2,3,4,5,5]))
