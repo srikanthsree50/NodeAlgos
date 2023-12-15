11dab953847c24329f02a36ce6bf92d43fe27316
srikanthsree50 <srikanthsree50@gmail.com>
2023-12-15 19:51:53 +0530

    stacks commit

---
+++ Stack.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+class Stack{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last  = newNode;
+        }
+        else{
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+var stack = new Stack();
+stack.push(10);
+stack.push(20)
+stack.push(30);
+stack.pop()
+console.log(stack);
